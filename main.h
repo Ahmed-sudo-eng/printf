57d384c8a966667c5fc865b8280ff84f18f804c9
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-19 10:50:30 +0200

    main.h file modified

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _printf(const char *format, ...);
-int write_char(char c);
+int write_char(unsigned char c);
 int write_str(char *s);
 int write_in_decimal(signed int number);
 int write_unsigned_decimal(unsigned int number);
